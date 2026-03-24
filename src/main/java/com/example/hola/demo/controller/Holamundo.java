dd2b23457cf83f92c995bc19b312b12109d40dc3
Jorge Zuñiga <jzuniga1983@gmail.com>
2026-03-24 19:54:31 -0300

    tercer commit

--- src/main/java/com/example/hola/demo/controller/Holamundo.java
+++ src/main/java/com/example/hola/demo/controller/Holamundo.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.Map;
 
 @RestController
-@RequestMapping("api/v1")
+@RequestMapping("api/v2")
 public class Holamundo {
 
     @GetMapping("/hola")
